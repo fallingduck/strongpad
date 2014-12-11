ce53c14784aa9809497aa031fb9d316d5e3c28b8
Jack <jack@fallingduck.net>
2014-12-10 21:34:18 -0800

    Finished login system

--- src/password.py
+++ src/password.py
@@ -6,5 +6,7 @@ import binascii
 def encrypt(password, salt=None):
     if salt is None:
         salt = os.urandom(8)
+    else:
+        salt = binascii.unhexlify(salt)
     hashed = scrypt.hash(password, salt)
     return binascii.hexlify(hashed), binascii.hexlify(salt)
--- src/server.py
+++ src/server.py
@@ -2,7 +2,48 @@ from SocketServer import ThreadingMixIn
 from wsgiref.simple_server import WSGIServer, make_server
 
 import bottle
+import json
+import binascii
+
 import sessions
+import password
+
+
+@bottle.route('/')
+@bottle.view('login')
+@sessions.start
+def login_page(session):
+    if session.get('in'):
+        bottle.redirect('/index')
+    return {}
+
+
+@bottle.route('/', method='POST')
+@sessions.start
+def process_login(session):
+    if session.get('in'):
+        bottle.redirect('/index')
+    p = bottle.request.forms.get('password')
+    with open('config.json') as f:
+        passw, salt = json.load(f)['password']
+    hashed = password.encrypt(p, salt)[0]
+    if hashed == passw:
+        session['in'] = True
+        bottle.redirect('/index')
+    bottle.redirect('/')
+
+
+@bottle.route('/index')
+@bottle.view('index')
+@sessions.start
+def serve_index(session):
+    if not session.get('in'):
+        bottle.redirect('/')
+
+
+@bottle.route('/static/<filename>')
+def serve_static(filename):
+    return bottle.static_file(filename, root='./views/static')
 
 
 @bottle.error(404)
