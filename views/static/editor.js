10d4206265e2fa6987bc82dfa56b8db3918c27e8
Jack <jack@fallingduck.net>
2014-12-11 22:33:35 -0800

    Friendlier UI for editor

--- views/static/editor.js
+++ views/static/editor.js
@@ -2,4 +2,5 @@ var editor = document.getElementById('editor');
 var viewer = document.getElementById('viewer');
 editor.onkeyup = function() {
     viewer.innerHTML = marked(editor.innerText);
+    viewer.scrollTop = viewer.scrollHeight;
 };
